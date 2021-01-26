c8517ea9dbeb793baa5e0224ade86fd7d8206318
Vadim <vbalan@mail.greenriver.edu>
2021-01-26 12:08:40 -0800

    Add project files.

---
+++ BasicSort/Algorithms.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicSort
+{
+    class Algorithms
+    {
+        /*
+        * @param array This is the bubbleSort algorithm that compares adjacent
+        * elements and swaps if they are in the wrong order and takes a random generated array
+        */
+        public void bubblesSort(int[] array)
+        {
+            int size = array.Length;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int x = 0; x < size - i - 1; x++)
+                {
+                    if (array[x] > array[x + 1])
+                    {
+                        int temp = array[x];
+                        array[x] = array[x + 1];
+                        array[x + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        /**
+        * @param array takes in a random generated array and sorts based on the selectionSort algorithm
+        */
+        public void selectionSort(int[] array)
+        {
+            int size = array.Length;
+            for (int i = 0; i < size - 1; i++)
+            {
+                int min = i;
+
+                for (int x = i + 1; x < size; x++)
+                {
+                    if (array[x] < array[min])
+                    {
+                        min = x;
+                    }
+                }
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+        }
+
+        /**
+        * @param array Takes a random generated array and sorts based on the insertionSort algorithm
+        */
+        public void insertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int x = i; x >= 1; x--)
+                {
+                    if (array[x] < array[x - 1])
+                    {
+                        swap(array, x, x - 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        /**
+        * @param array Takes in a random generated array
+        * @return returns if the array has inversions
+        */
+        public static bool hasInversions(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+        * @param array Takes in a random generated array
+        * @param first is the first number that needs to be swapped with the second number
+        * @param second this is the second number
+        */
+        public static void swap(int[] array, int first, int second)
+        {
+            int temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+
+    }
+}
---
+++ BasicSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BasicSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Algorithms sort = new Algorithms();
+            // You can change the size here: size, min, max
+            int[] test = genArray(1000, 1, 5000);
+            
+            // Different sorting algorithms 
+            // Comment two of the three out to test one of the sorting algorithms
+            sort.bubblesSort(test);
+            sort.insertionSort(test);
+            sort.selectionSort(test);
+
+            // Line the array up using ", " 
+            string testString = string.Join(", ", test);
+            // Print out the algorithm 
+            Console.WriteLine(testString);
+            Console.WriteLine("Inversions? " + Algorithms.hasInversions(test));
+        }
+
+        // Generates a random array with the size of the array, the min number to use in array
+        // and max number to use in Array
+        public static int[] genArray(int size, int min, int max)
+        {
+            Random rand = new Random();
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = min + rand.Next(max - min + 1);
+            }
+
+            return array;
+        }
+    }
+}
